07b340ed811a580d0ed53e09c58fd191ade053b8
Turner Max <voloshok.maksi@ukr.net>
2023-07-17 20:05:59 +0300

    calendar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CardController;
@@ -46,6 +46,9 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('Calendar/Calendar') ;
     });
 
+    Route::get('/calendar/calendar', [CalendarController::class, 'index']);
+
+
     Route::get('/current-desk', [CardController::class, 'currentDesk']);
 //    Route::get('/current-desk',function (){
 //        return Inertia::render('CurrentDesk/CurrentDesk') ;
@@ -75,6 +78,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/usercomponent/Index',[UserController::class,'index'])->name('UserComponent.Index');
     Route::post('/usercomponent/findByEmail',[UserController::class,'findByEmail'])->name('UserComponent.findByEmail');
 });
+
 
 
 
