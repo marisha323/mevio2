401d7acac8d9405d121b9b6b1aeffe8670570ba0
YehorBelenkov <yehorbelenkov@gmail.com>
2023-07-18 23:18:49 -0500

    almost completed the invitation of the users, need to merge branches with the user db egor where I implemented deletion of the user

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -11,6 +11,9 @@ use App\Repositories\CardRepository;
 use Inertia\Inertia;
 use Inertia\Response;
 
+use App\Models\User;
+use App\Models\DesksUsers;
+
 
 class CardController extends Controller
 {
@@ -36,14 +39,31 @@ class CardController extends Controller
        //return Inertia::render('CurrentDesk/CurrentDesk');
     }
 
-    public function currentDesk(Request $request):Response
+//     public function currentDesk(Request $request):Response
+//     {
+//         $desk_id = $request->get('desk_id');
+//         $cards=$this->cardModel->currentDesk();
+//         return Inertia::render('CurrentDesk/CurrentDesk', ['cards' => $cards]);
+// //        return Inertia::render('CurrentDesk/CurrentDesk');
+//     }
+
+    public function currentDesk(Request $request): Response
     {
         $desk_id = $request->get('desk_id');
-        $cards=$this->cardModel->currentDesk();
-        return Inertia::render('CurrentDesk/CurrentDesk', ['cards' => $cards]);
-//        return Inertia::render('CurrentDesk/CurrentDesk');
-    }
 
+        // Fetch users who do not exist in the given desk using the doesNotHave relationship.
+        $users = User::whereDoesntHave('desks', function ($query) use ($desk_id) {
+            $query->where('desk_id', $desk_id);
+        })->get();
+
+        // Fetch the cards associated with the desk. I'm assuming you already have a method in your cardModel for this.
+        $cards = $this->cardModel->currentDesk();
+
+        return Inertia::render('CurrentDesk/CurrentDesk', [
+            'cards' => $cards,
+            'users' => $users,
+        ]);
+    }
 
     public function destroy(){
 
--- resources/js/Pages/CurrentDesk/CurrentDesk.jsx
+++ resources/js/Pages/CurrentDesk/CurrentDesk.jsx
@@ -1,45 +1,141 @@
 import '../../../css/current_desk/current_desk.css';
 
-import {DashBoardLayout} from "@/Layouts/DashBoardLayout.jsx";
-import {Link} from "@inertiajs/react";
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Card, Board } from '@/React-dnd-Components';
+import axios from 'axios';
+import { DashBoardLayout } from '@/Layouts/DashBoardLayout.jsx';
+import { Link } from '@inertiajs/react';
 
-export default function CurrentDesk({cards}) {
+export default function CurrentDesk({ cards, users }) {
 
-    const redirectToCreateCard = () => {
-       // console.log(111);
-        window.location.href = '/create-card';
-    };
-    const cardRef = useRef(null);
+  const [searchValue, setSearchValue] = useState('');
 
-    const handleDragEnd = () => {
-      const card = cardRef.current;
-      const parent = card.parentElement;
-      const board = parent.closest('.board');
-  
-      if (!board) {
-        // If the card is dragged outside the Board, append it back to its original parent
-        const originalBoard = document.getElementById(card.dataset.originalBoard);
-        if (originalBoard) {
-          originalBoard.appendChild(card);
-        }
+  //filter user display search
+  const [filteredUsers, setFilteredUsers] = useState(users);
+
+  //needed to remove text later from the search bar
+  const [notification, setNotification] = useState(null);
+
+  useEffect(() => {
+    // Filter the users based on the searchValue
+    const filtered = users.filter(
+      (user) => user.email.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  }, [searchValue, users]);
+
+  const redirectToCreateCard = () => {
+    window.location.href = '/create-card';
+  };
+
+  //needed for card display
+  const cardRef = useRef(null);
+
+  //drag and drop
+  const handleDragEnd = () => {
+    const card = cardRef.current;
+    const parent = card.parentElement;
+    const board = parent.closest('.board');
+
+    if (!board) {
+      const originalBoard = document.getElementById(card.dataset.originalBoard);
+      if (originalBoard) {
+        originalBoard.appendChild(card);
       }
+    }
+  };
+
+  //Display the toggle invite div
+  const ToggleInviteDesk = () => {
+    const inv_desk = document.getElementsByClassName('Add_User_Overlayer')[0];
+    const overlayer = document.getElementsByClassName(
+      'Add_User_Background_Overlayer'
+    )[0];
+
+    if (overlayer.style.display == 'none' || overlayer.style.display == '') {
+      inv_desk.style.display = 'block';
+      overlayer.style.display = 'block';
+    } else {
+      inv_desk.style.display = 'none';
+      overlayer.style.display = 'none';
+    }
+  };
+
+  const sendEmail = async () => {
+    if (searchValue.trim() === '') {
+      // Empty search value, do not send email
+      return;
+    }
+
+    const email = searchValue;
+    const data = {
+      email,
     };
+
+    try {
+      const response = await axios.post(
+        'https://send-email-invitation-api.onrender.com/api/sendemail',
+        data
+      );
+      //displays email sent!
+      console.log(response.data);
+      setNotification({
+        type: 'success',
+        message: 'Success! Your email has been sent.',
+      });
+      setSearchValue(''); // Clear the search bar
+    } catch (error) {
+      console.error(error);
+      setNotification({
+        type: 'error',
+        message: 'An error occurred while sending the email.',
+      });
+    }
+  };
 
   return (
     <DashBoardLayout>
-      <div className="middle_desks_container">
-        <div className="middle_top_body_tasks">
+      <div className='Add_User_Background_Overlayer' onClick={ToggleInviteDesk}></div>
+      <div className='middle_desks_container'>
+        <div className='middle_top_body_tasks'>
           <h1>Курсовой проект</h1>
-          <div className="tasks_user_profiles">
-            <img src="images/profile1.png" alt="" />
-            <img src="images/profile2.png" alt="" />
-            <img src="images/profile3.png" alt="" />
-            <img src="images/Ellipse 11.png" alt="" />
+          <div className='tasks_user_profiles'>
+            <img src='images/profile1.png' alt='' />
+            <img src='images/profile2.png' alt='' />
+            <img src='images/profile3.png' alt='' />
+            <img src='images/Ellipse 11.png' alt='' />
           </div>
-          <button>Поделиться</button>
-          <img src="images/settings (1) 1.png" alt="" />
+          <button onClick={ToggleInviteDesk}>Поделиться</button>
+          <img src='images/settings (1) 1.png' alt='' />
+        </div>
+        <div className='Add_User_Overlayer'>
+          <h1 className='add_user_desk_h1'>Додати користувача до дошки</h1>
+          <div className='Search_engine_add_desk'>
+            <input
+              type='text'
+              className='Search_user_add'
+              placeholder='Пошук...'
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <button className='Add_unk_user' onClick={sendEmail}>
+              Запросіть
+            </button>
+          </div>
+
+          <div className='User_Add_List_Container'>
+            <hr />
+            {filteredUsers.map((user) => (
+              <React.Fragment key={user.email}>
+                <div className='User_Add_Container'>
+                  <img className='add_user_pfp' src='{image}' alt='' />
+                  <p>{user.email}</p>
+                  <button className='add_btn_user_desk'>Додати</button>
+                </div>
+                <hr />
+              </React.Fragment>
+            ))}
+          </div>
         </div>
         <div className="main-container">
           <ul className="columns">
@@ -63,7 +159,7 @@ export default function CurrentDesk({cards}) {
                   </Card>
               </div>
                 ))}
-                {/* <Card
+                <Card
                 id="card-1"
                 className="task"
                 draggable="true"
@@ -72,7 +168,7 @@ export default function CurrentDesk({cards}) {
                 data-original-board="to-do"
                 > 
                   <p>Card one</p>
-                </Card> */}
+                </Card>
                 
               </Board>
                 <Link href="/create-card?columnId=1">
@@ -137,5 +233,3 @@ export default function CurrentDesk({cards}) {
     </DashBoardLayout>
   );
 }
-
-
