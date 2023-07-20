c3cae14f0307fe1b26660af428520209c8f38cff
YehorBelenkov <yehorbelenkov@gmail.com>
2023-07-20 14:22:01 -0500

    Chaging errors in the User_All_Egor

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,6 +3,9 @@ namespace App\Repositories;
 
 use App\Contracts\UserContract;
 use App\Models\User;
+use App\Models\Desk;
+use App\Models\DesksUsers;
+use App\Models\Theme;
 use Inertia\Inertia;
 
 
@@ -24,8 +27,41 @@ class UserRepository implements UserContract
         return Inertia::render('UserComponent/Index');
     }
     public function returnUsers()
+{
+    $loggedInUserId = auth()->user()->id;
+
+    // Get the desks of the authenticated user
+    $userDesks = Desk::whereHas('users', function ($query) use ($loggedInUserId) {
+            $query->where('users.id', $loggedInUserId);
+        })
+        ->get();
+
+    $deskIds = $userDesks->pluck('id')->toArray();
+
+    // Get users who belong to the same desks as the authenticated user
+    $usersInSameDesks = User::whereHas('desks', function ($query) use ($deskIds) {
+            $query->whereIn('desks.id', $deskIds);
+        })
+        ->get();
+
+    $themes = Theme::all();
+    // dd($userDesks);
+
+    return [
+        'loggedInUserId' => $loggedInUserId,
+        'desks' => $userDesks,
+        'desksusers' => $deskIds,
+        'users' => $usersInSameDesks,
+        'themes' => $themes
+    ];
+}
+public function delete($userId, $deskId)
     {
-        return User::all();
+        //removing user from desk!
+        $deskUser = DesksUsers::where('userId', $userId)->where('deskId', $deskId)->first();
+        if ($deskUser) {
+            $deskUser->delete();
+        }
     }
     public function findByEmail($email)
     {
@@ -44,11 +80,6 @@ class UserRepository implements UserContract
         $user->save();
 
         return $user;
-    }
-
-    public function delete($id)
-    {
-        return User::destroy($id);
     }
 
 
--- resources/js/Pages/Users/Users.jsx
+++ resources/js/Pages/Users/Users.jsx
@@ -1,146 +1,115 @@
-
 import "../../../css/users/users.css";
-import {DashBoardLayout} from "@/Layouts/DashBoardLayout.jsx";
+import { DashBoardLayout } from "@/Layouts/DashBoardLayout.jsx";
+import React, { useState } from 'react';
+import { InertiaLink } from "@inertiajs/inertia-react";
 
-export default function Users() {
+export default function Users({ users, desks, desksusers, loggedInUserId, themes }) {
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedBoard, setSelectedBoard] = useState(null);
+  const [filteredDesks, setFilteredDesks] = useState([]);
 
-    return (
-        <DashBoardLayout>
-            <div className="middle_desks_container">
-                <div className="middle_top_body_users">
-                    <div className="working_space">
-                        <div className="working_space_img">
-                            <img src="images/workingspaceborderfirst.png" alt=""/>
-                            <img src="images/workingspace.png" alt=""/>
-                            <img src="images/workingspaceborder.png" alt=""/>
-                        </div>
-                        <div className="workingspace_title">
-                            <h3>Робочий простір Mevio <img src="images/edit_pen.png" alt=""/></h3>
-                            <p><img src="images/graylock.png" alt=""/>Приватно</p>
-                        </div>
-                        <button><img src="images/inv_peopleIcon.png" alt=""/> Заглушити в робочий простір</button>
-                    </div>
-                </div>
-                <div className="middle_middle_body">
-                    <div className="users_container">
-                        <h3>Учасники робочого простору(?)</h3>
-                        <p>Учасники робочого простору можуть переглядати дошки для робочого простору та
-                            приєднуватися до них і створювати дошки в цьому просторі</p>
-                        <input type="text" placeholder="Фільтр за іменами"/>
-                        <div className="users_list">
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Адміністратор</button>
-                                    <button className="remove_user_btn"><img src="images/x_icon.png"
-                                                                             alt=""/> Виключити
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Администратор</button>
-                                    <button className="remove_user_btn"><img src="images/x_icon.png"
-                                                                             alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Администратор</button>
-                                    <button className="remove_user_btn"><img src="images/x_icon.png"
-                                                                             alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Администратор</button>
-                                    <button className="remove_user_btn"><img
-                                        src="images/x_icon.png" alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Администратор
-                                    </button>
-                                    <button className="remove_user_btn"><img
-                                        src="images/x_icon.png" alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button className="admin_user_btn">Администратор
-                                    </button>
-                                    <button className="remove_user_btn"><img
-                                        src="images/x_icon.png" alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="list_user">
-                                <img src="images/profile1.png" alt=""/>
-                                <div className="user_name">
-                                    <h4>Name</h4>
-                                    <p>@name1234</p>
-                                </div>
-                                <div className="user_list_control">
-                                    <h4>На доске(?)</h4>
-                                    <button
-                                        className="admin_user_btn">Администратор
-                                    </button>
-                                    <button className="remove_user_btn"><img
-                                        src="images/x_icon.png" alt=""/> Исключить
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+
+    setFilteredUsers(() => {
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    });
+  };
+
+  const handleSelectBoard = (board) => {
+    setSelectedBoard(board);
+
+    setFilteredUsers(() => {
+      return users.filter((user) =>
+        desksusers.some((deskuser) => deskuser.deskId === board.id && deskuser.userId === user.id)
+      );
+    });
+    console.log(filteredUsers);
+  };
+
+  const handleRemoveUser = (userId) => {
+    try {
+      const updatedUsers = filteredUsers.filter((user) => user.id !== userId);
+      setFilteredUsers(updatedUsers);
+
+      const deskIndex = filteredDesks.findIndex(
+        (deskuser) => deskuser.userId === userId && deskuser.deskId === selectedBoard.id
+      );
+
+      if (deskIndex !== -1) {
+        const updatedDesksUsers = [...filteredDesks];
+        updatedDesksUsers.splice(deskIndex, 1);
+        setFilteredDesks(updatedDesksUsers);
+      }
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
+
+  return (
+    <DashBoardLayout>
+      <div className="middle_desks_container">
+        <div className="middle_middle_body">
+          <div className="users_container">
+            <div className='Boards_Container'>
+              {desks.map((desk) => {
+                const theme = themes.find((theme) => theme.id === desk.themeId);
+                return (
+                  <div key={desk.id} className='BoardListed' onClick={() => handleSelectBoard(desk)}>
+                    <img className='BoardListed_img' src={theme.backGroundImage} alt="" />
+                    <h1 className='BoardListed_name'>{desk.deskName}</h1>
+                  </div>
+                );
+              })}
             </div>
-        </DashBoardLayout>
-    )
+            <input
+              type="text"
+              placeholder="Фільтр за іменами"
+              value={searchTerm}
+              onChange={handleSearch}
+            />
+            {selectedBoard && (
+              <div className="users_list">
+                <hr />
+                {filteredUsers.map((user) => (
+                  <div key={user.id} className="list_user">
+                    <img src="images/profile1.png" alt="" />
+                    <div className="user_name">
+                      <h4>{user.name}</h4>
+                      <p>{user.email}</p>
+                    </div>
+                    <div className="user_list_control">
+                      <h4>На доске(?)</h4>
+                      {loggedInUserId === user.id ? (
+                        <button className="admin_user_btn">Адміністратор</button>
+                      ) : (
+                        <button className="admin_user_btn">Користувач</button>
+                      )}
+                      {loggedInUserId === selectedBoard.userId ? (
+                        <InertiaLink
+                          className="remove_user_btn"
+                          onClick={() => handleRemoveUser(user.id)}
+                          href={`/users/${user.id}/desk/${selectedBoard.id}`}
+                          method="delete"
+                          as="button"
+                          type="button"
+                        >
+                          <img src="images/x_icon.png" alt="" /> Виключити
+                        </InertiaLink>
+                      ) : null}
+                    </div>
+                  </div>
+                ))}
+                <hr />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </DashBoardLayout>
+  );
 }
